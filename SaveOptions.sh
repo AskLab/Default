c72c1b29359221950733d86926ff5baed3113fe4
080027e1651d <mail@student.pg.gda.pl>
2016-01-19 02:24:37 +0100

    MainCommit

--- SaveOptions.sh
+++ SaveOptions.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+repo=/home/root/usr/AskRepo
 mac=$(cat /sys/class/net/eth0/address)
 var=${mac//:}
 currentTime=$(date +"%m-%d-%y_%H%M")
 
 #Copy current options to repo
 cp -f /etc/network/interfaces $repo
-cd /home/root/usr/AskRepo/
+cd $repo
 
 #Check for changes and do action
 output=$(git status)
 if [[ $output == *"interfaces"* ]]
 then
-	git checkout -b var
+	git checkout $var
 	git add interfaces
 	git commit -m "$currentTime"
-	git push -u origin $var
-	echo "Updated new options on branch: $var with commit message: $currenttTime"
+	git push -f origin $var
+	echo "Updated new options on branch: $var with commit message: $currentTime"
 else
 	echo "Options up to date"
 fi
